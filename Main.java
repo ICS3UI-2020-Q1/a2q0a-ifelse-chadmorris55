13176f5dda870a0ad7922ba5e2dae24f38a2fce9
morrc3611 <morrc3611@wrdsb.ca>
2020-09-14 15:36:55 +0000

    drive/vote check

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  *
  * @author 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //license age
+    final int LICENSE_AGE = 16;
+    final int VOTING_AGE = 18;
+    // create Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //asks user for age
+    System.out.println("Please enter your age:");
+    int age = input.nextInt();
     
+    //checks if user and drive, vote, or both
+    if (age>= VOTING_AGE){
+      System.out.println("You are old enough to drive and vote!");
+    } else if (age >= LICENSE_AGE){
+      System.out.println("You can get your driver's license!");
+      } else {
+        System.out.println("Sorry, you are not old enough to drive or vote.");
+}
   }
 }
